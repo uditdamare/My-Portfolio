18cbfaedccb9cd886913836a82ea30a406594f2d
Udit Damare <Udit.Damare@aurumproptech.in>
2025-02-27 11:48:51 +0530

    profile chnages

--- Frontend/Profile.js
+++ Frontend/Profile.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+
+// import lc from ''
 export default function Profile() {
     return (<>
         <div className="lg:min-w-[50%] border-2 border-red-400 text-white">
@@ -7,9 +10,48 @@ export default function Profile() {
                 <p className="text-[16px] mb-3 max-w-[80%]">Experienced MERN Stack Developer with 2 years of expertise in building scalable web applications, optimizing SEO and Core Web Vitals, and developing microservices using Spring Boot for backend solutions. </p>
                 <nav class="nav hidden lg:block" aria-label="In-page jump links"><ul class="mt-16 w-max"><li><a class="group flex items-center py-3 active" href="#about"><span class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span><span class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">About</span></a></li><li><a class="group flex items-center py-3" href="#experience"><span class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span><span class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">Experience</span></a></li><li><a class="group flex items-center py-3" href="#projects"><span class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span><span class="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">Projects</span></a></li></ul></nav>
 
-                <div className="">
+                <div className="connectMe secton">
+                    <ul class="ml-1 mt-8 flex items-center" aria-label="Social media">
+                        <li class="mr-5 shrink-0 text-xs">
+                            <a class="block hover:text-slate-200" href="https://www.linkedin.com/in/udit-damare/" target="_blank" rel="noreferrer noopener" aria-label="LinkedIn (opens in a new tab)" title="LinkedIn">
+                                <span class="sr-only">
+                                    LinkedIn
+                                </span>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6" aria-hidden="true">
+                                    <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="mr-5 shrink-0 text-xs">
+                            <a class="block hover:text-slate-200" href="https://github.com/uditdamare" target="_blank" rel="noreferrer noopener" aria-label="GitHub (opens in a new tab)" title="GitHub">
+                                <span class="sr-only">
+                                    GitHub
+                                </span>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="h-6 w-6" aria-hidden="true">
+                                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+                                    ></path>
+                                </svg>
+                            </a>
+                        </li>
 
-                    <ul class="ml-1 mt-8 flex items-center" aria-label="Social media"><li class="mr-5 shrink-0 text-xs"><a class="block hover:text-slate-200" href="https://github.com/bchiang7" target="_blank" rel="noreferrer noopener" aria-label="GitHub (opens in a new tab)" title="GitHub"><span class="sr-only">GitHub</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="h-6 w-6" aria-hidden="true"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg></a></li><li class="mr-5 shrink-0 text-xs"><a class="block hover:text-slate-200" href="https://www.linkedin.com/in/bchiang7/" target="_blank" rel="noreferrer noopener" aria-label="LinkedIn (opens in a new tab)" title="LinkedIn"><span class="sr-only">LinkedIn</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6" aria-hidden="true"><path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path></svg></a></li><li class="mr-5 shrink-0 text-xs"><a class="block hover:text-slate-200" href="https://codepen.io/bchiang7" target="_blank" rel="noreferrer noopener" aria-label="CodePen (opens in a new tab)" title="CodePen"><span class="sr-only">CodePen</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6" aria-hidden="true"><path d="M3.06 41.732L32 60.932l28.94-19.2V22.268L32 3.068l-28.94 19.2zm57.878 0L32 22.268 3.06 41.732m0-19.463L32 41.47l28.94-19.2M32 3.068v19.2m0 19.463v19.2" stroke-width="5"></path></svg></a></li>
+                        <li class="mr-5 shrink-0 text-xs">
+                            <a class="block hover:text-slate-200" href="https://leetcode.com/u/uditdamare01/" target="_blank" rel="noreferrer noopener" aria-label="LeetCode (opens in a new tab)" title="LeetCode">
+                                <span class="sr-only">
+                                    Leetcode
+                                </span>
+                                <Image className=" text-white h-7 w-7" src={"/Assets/lc.svg"} alt="leetcode"
+                                    width={50}
+                                    height={50}
+                                />
+                                {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6" aria-hidden="true">
+                                    <path d="M3.06 41.732L32 60.932l28.94-19.2V22.268L32 3.068l-28.94 19.2zm57.878 0L32 22.268 3.06 41.732m0-19.463L32 41.47l28.94-19.2M32 3.068v19.2m0 19.463v19.2" stroke-width="5">
+                                    </path>
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path fill="#ffffff" d="M496 64H16C7.163 64 0 71.163 0 80v352c0 8.837 7.163 16 16 16h480c8.837 0 16-7.163 16-16V80c0-8.837-7.163-16-16-16zm-48 352H64V128h384v288zM256 192c-35.346 0-64 28.654-64 64s28.654 64 64 64 64-28.654 64-64-28.654-64-64-64zm0 96c-17.673 0-32-14.327-32-32s14.327-32 32-32 32 14.327 32 32-14.327 32-32 32z" />
+                                </svg> */}
+                            </a>
+                        </li>
                         <li class="mr-5 shrink-0 text-xs">
                             <a class="block hover:text-slate-200" href="https://instagram.com/bchiang7" target="_blank" rel="noreferrer noopener" aria-label="Instagram (opens in a new tab)" title="Instagram">
                                 <span class="sr-only">
@@ -21,7 +63,7 @@ export default function Profile() {
                                 </svg>
                             </a>
                         </li>
-                        <li class="mr-5 shrink-0 text-xs">
+                        {/* <li class="mr-5 shrink-0 text-xs">
                             <a class="block hover:text-slate-200" href="https://www.goodreads.com/user/show/143480747-brittany-chiang" target="_blank" rel="noreferrer noopener" aria-label="Goodreads (opens in a new tab)" title="Goodreads">
                                 <span class="sr-only">Goodreads</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" fill="currentColor" class="h-6 w-6" aria-hidden="true">
@@ -29,7 +71,7 @@ export default function Profile() {
                                     </path>
                                 </svg>
                             </a>
-                        </li>
+                        </li> */}
                     </ul>
 
                 </div>
