9254e6f232345d7c79da255313877e8186209877
Udit Damare <Udit.Damare@aurumproptech.in>
2025-02-27 14:19:39 +0530

    details page started

--- Frontend/Details.js
+++ Frontend/Details.js
@@ -1,8 +1,9 @@
-export default function Details(){
-    return(<>
-    <div className="lg:min-w-[50%] h-[2000px]">
+import AboutMe from "./DetailsComponents/AboutMe";
 
-    DEtails
-    </div>
-     </>)
+export default function Details() {
+    return (<>
+        <div className="lg:min-w-[50%] h-[2000px]  pt-10 px-10 border-2 border-red-500">
+            <AboutMe />
+        </div>
+    </>)
 }
---
+++ Frontend/DetailsComponents/AboutMe.js
@@ -0,0 +1,20 @@
+import React from "react"
+export default function AboutMe() {
+
+    return (
+        <>
+            <div className="">
+                <p className="mb-4 border-2 border-black">
+                    I’m a MERN developer passionate about building performant, accessible, and scalable web applications. I thrive at the intersection of design and development, crafting seamless user experiences that balance aesthetics with efficiency. With a strong foundation in JavaScript, TypeScript, and modern frontend frameworks, I focus on delivering clean, maintainable, and high-quality code.
+                </p>
+                <p className="mb-4">
+                    Beyond coding, I enjoy diving into emerging technologies, analyzing chess strategies, and exploring ways to enhance website performance. When I’m not optimizing code, you can find me playing chess, experimenting with new tech stacks, or strategizing my next e-commerce project.
+                </p>
+                <p className="mb-4">
+
+                    Let’s connect and build something amazing together!
+                </p>
+            </div>
+
+        </>)
+}
