898d5a126cd085552f7fce5ff0c3c6010d8b3be0
Udit Damare <Udit.Damare@aurumproptech.in>
2025-03-18 19:10:54 +0530

    experience started

--- Frontend/Details.js
+++ Frontend/Details.js
@@ -1,9 +1,11 @@
 import AboutMe from "./DetailsComponents/AboutMe";
+import Experience from "./DetailsComponents/Experience";
 
 export default function Details() {
     return (<>
         <div className="lg:min-w-[50%] h-[2000px]  pt-10 px-10 border-2 border-red-500">
             <AboutMe />
+            <Experience/>
         </div>
     </>)
 }
---
+++ Frontend/DetailsComponents/Experience.js
@@ -0,0 +1,4 @@
+export default function Experience(){
+    return (<>
+    </>)
+}
