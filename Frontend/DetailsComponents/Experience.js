33ef8c55be8cfaeef8f6676ecfa9b9ad69627564
Udit Damare <Udit.Damare@aurumproptech.in>
2025-03-24 14:54:01 +0530

    saving

--- Frontend/DetailsComponents/Experience.js
+++ Frontend/DetailsComponents/Experience.js
@@ -4,7 +4,7 @@ export default function Experience() {
     const exprecord = [
         {
             date: "Jul-2022 - Present",
-            post: "Associate Software Engineer",
+            post: "AurumLiv",
             content: "enerating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a sho",
             tags: ["react", "node"]
         },
@@ -37,7 +37,7 @@ export default function Experience() {
                 href={"https://drive.google.com/file/d/1HWG0kbRYHltJLCESucuycVpk-0hM3oht/view?usp=sharing"}
                 target="_blank"
                 rel="noopener noreferrer"
-                className=" text-white font-semibold w-fit px-4 duration-300 flex
+                className=" text-white font-semibold w-fit px-4 duration-300 flex text-[20px]
                 hover:text-purple-400 transition-colors
                 "
             >
@@ -61,7 +61,7 @@ export default function Experience() {
 const ExperienceCard = ({ data }) => {
     return (<>
         <div className="card flex mb-4">
-            <div className="left max-w-[35%] min-w-[35%] text-[#7f7d7f] text-center flex justify-center pt-1">
+            <div className="left max-w-[35%] min-w-[35%] text-[#7f7d7f] text-center flex justify-start pt-1 ">
                 {data.date}
             </div>
             <div className="right  max-w-[65%] min-w-[65%]">
