be2ae898ce65da2103ece773d28a41f09551275f
uditdamare <uditdamare01@gmail.com>
2025-04-07 23:21:58 +0530

    more sections added

--- Frontend/Details.js
+++ Frontend/Details.js
@@ -1,5 +1,6 @@
 import AboutMe from "./DetailsComponents/AboutMe";
 import Experience from "./DetailsComponents/Experience";
+import LeetCode from "./DetailsComponents/LeetCode";
 import Projects from "./DetailsComponents/Projects";
 
 export default function Details() {
@@ -7,6 +8,7 @@ export default function Details() {
         <div className="lg:min-w-[50%] h-[2000px]s  pt-10 px-10 ">
             <AboutMe />
             <Experience />
+            <LeetCode/>
             <Projects/>
             
         </div>
--- Frontend/Profile.js
+++ Frontend/Profile.js
@@ -35,7 +35,16 @@ export default function Profile() {
                             </a>
                         </li>
                         <li>
-                            <a class="group flex items-center py-3" >
+                            <a class="group flex items-center py-3" onClick={() => { scrollToDiv('leetcode') }}>
+                                <span class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none">
+                                </span><span class="nav-text text-xs font-bold uppercase tracking-widest text-white group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                                    LeetCode
+
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="group flex items-center py-3"onClick={() => { scrollToDiv('projects') }} >
                                 <span class="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none">
                                 </span><span class="nav-text text-xs font-bold uppercase tracking-widest text-white group-hover:text-slate-200 group-focus-visible:text-slate-200">
                                     Projects
